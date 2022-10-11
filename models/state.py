4327c3242442427edb8e865e23c63144cab08c1a
osewus <34625293+osewus@users.noreply.github.com>
2022-10-11 13:00:33 +0000

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,35 +1,23 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-import models
+"""This is the state class"""
 from models.base_model import BaseModel, Base
 from models.city import City
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
+import models
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """ State class """
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state")
+    cities = relationship("City", cascade="all, delete, delete-orphan",
+                          backref="state")
 
-    def __init__(self, *args, **kwargs):
-        """
-        init inherited
-        """
-        super().__init__(*args, **kwargs)
-
-    if models.storage_type != "db":
-        @property
-        def cities(self):
-            """getter for cities that return
-            a list of city instance equale to
-            curent state id
-            """
-            list_city = []
-            all_inst_c = models.storage.all(City)
-            for value in all_inst_c.values():
-                if value.state_id == self.id:
-                    list_city.append(value)
-            return list_city
+    @property
+    def cities(self):
+        objs = models.storage.all(City)
+        return [v for k, v in objs.items() if v.state_id == self.id]
