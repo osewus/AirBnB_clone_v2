aaaecabd966b2b318b48ec85b82fe48f5c51d09a
osewus <34625293+osewus@users.noreply.github.com>
2022-11-14 01:23:33 +0000

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
-""" Script that runs a Flask app """
-from flask import Flask, render_template
+"""Starts a Flask web application"""
+
 from models import storage
+from models.state import State
+from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 
+@app.route('/cities_by_states', strict_slashes=False)
+def cities():
+    """Returns a rendered html template
+    at the /cities_by_states route,
+    listing the cities by states"""
+    return render_template('8-cities_by_states.html',
+                           states=storage.all('State').values())
+
+
 @app.teardown_appcontext
-def closing(error):
-    """closes session """
+def teardown(self):
+    """Removes the current SQLAlchemy Session"""
     storage.close()
 
-
-@app.route('/cities_by_states', strict_slashes=False)
-def city():
-    """ function that returns text"""
-    state = storage.all('State')
-    return render_template('8-cities_by_states.html', state=state)
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
